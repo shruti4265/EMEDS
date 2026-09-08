e19d19d285ecff45efd5b29e6918ba20b47af681
Chhaya Sharma <chhaya.sharma1607@gmail.com>
2026-09-08 15:19:54 +0530

    Add NUnit unit tests for medicine and category

--- EMEDS.Tests/CategoryTests.cs
+++ EMEDS.Tests/CategoryTests.cs
@@ -1,10 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EMEDS_Project.Controllers;
+using EMEDS_Project.Models;
+using EMEDS_Project.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
 
 namespace EMEDS.Tests
 {
-    internal class CategoryTests
+    [TestFixture]
+    public class CategoryTests
     {
+        private Mock<ICategoryRepo> _categoryRepoMock;
+        private CategoryController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _categoryRepoMock = new Mock<ICategoryRepo>();
+            _controller = new CategoryController(_categoryRepoMock.Object);
+        }
+
+        [Test]
+        public void Index_ReturnsViewWithAllCategories()
+        {
+            // Arrange
+            var categories = new List<Category>
+            {
+                new Category(),
+                new Category()
+            };
+
+            _categoryRepoMock
+                .Setup(repo => repo.GetAllCategories())
+                .Returns(categories);
+
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult.Model, Is.EqualTo(categories));
+        }
+
+        [Test]
+        public void Details_WhenCategoryExists_ReturnsViewWithCategory()
+        {
+            // Arrange
+            int categoryId = 1;
+            var category = new Category();
+
+            _categoryRepoMock
+                .Setup(repo => repo.GetCategoryById(categoryId))
+                .Returns(category);
+
+            // Act
+            var result = _controller.Details(categoryId);
+
+            // Assert
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult.Model, Is.EqualTo(category));
+        }
+
+        [Test]
+        public void Details_WhenCategoryDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            int categoryId = 999;
+
+            _categoryRepoMock
+                .Setup(repo => repo.GetCategoryById(categoryId))
+                .Returns((Category?)null);
+
+            // Act
+            var result = _controller.Details(categoryId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void Create_WhenModelIsValid_AddsCategoryAndRedirectsToIndex()
+        {
+            // Arrange
+            var category = new Category();
+
+            // Act
+            var result = _controller.Create(category);
+
+            // Assert
+            _categoryRepoMock.Verify(
+                repo => repo.AddCategory(category),
+                Times.Once
+            );
+
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.That(redirectResult, Is.Not.Null);
+            Assert.That(
+                redirectResult.ActionName,
+                Is.EqualTo(nameof(CategoryController.Index))
+            );
+        }
     }
 }
--- EMEDS.Tests/MedicineTests.cs
+++ EMEDS.Tests/MedicineTests.cs
@@ -1,10 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EMEDS_Project.Controllers;
+using EMEDS_Project.Models;
+using EMEDS_Project.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
 
 namespace EMEDS.Tests
 {
-    internal class MedicineTests
+    [TestFixture]
+    public class MedicineTests
     {
+        private Mock<IMedicineRepo> _medicineRepoMock;
+        private MedicineController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _medicineRepoMock = new Mock<IMedicineRepo>();
+            _controller = new MedicineController(_medicineRepoMock.Object);
+        }
+
+        [Test]
+        public void Index_ReturnsViewWithAllMedicines()
+        {
+            // Arrange
+            var medicines = new List<Medicine>
+            {
+                new Medicine(),
+                new Medicine()
+            };
+
+            _medicineRepoMock
+                .Setup(repo => repo.GetAllMedicines())
+                .Returns(medicines);
+
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult.Model, Is.EqualTo(medicines));
+        }
+
+        [Test]
+        public void Details_WhenMedicineExists_ReturnsViewWithMedicine()
+        {
+            // Arrange
+            int medicineId = 1;
+            var medicine = new Medicine();
+
+            _medicineRepoMock
+                .Setup(repo => repo.GetMedicineById(medicineId))
+                .Returns(medicine);
+
+            // Act
+            var result = _controller.Details(medicineId);
+
+            // Assert
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult.Model, Is.EqualTo(medicine));
+        }
+
+        [Test]
+        public void Details_WhenMedicineDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            int medicineId = 999;
+
+            _medicineRepoMock
+                .Setup(repo => repo.GetMedicineById(medicineId))
+                .Returns((Medicine?)null);
+
+            // Act
+            var result = _controller.Details(medicineId);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void Create_WhenModelIsValid_AddsMedicineAndRedirectsToIndex()
+        {
+            // Arrange
+            var medicine = new Medicine();
+
+            // Act
+            var result = _controller.Create(medicine);
+
+            // Assert
+            _medicineRepoMock.Verify(
+                repo => repo.AddMedicine(medicine),
+                Times.Once
+            );
+
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.That(redirectResult, Is.Not.Null);
+            Assert.That(
+                redirectResult.ActionName,
+                Is.EqualTo(nameof(MedicineController.Index))
+            );
+        }
+
+        [Test]
+        public void Search_ReturnsIndexViewWithSearchResults()
+        {
+            // Arrange
+            string searchTerm = "Paracetamol";
+
+            var medicines = new List<Medicine>
+            {
+                new Medicine()
+            };
+
+            _medicineRepoMock
+                .Setup(repo => repo.SearchMedicines(searchTerm))
+                .Returns(medicines);
+
+            // Act
+            var result = _controller.Search(searchTerm);
+
+            // Assert
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult.ViewName, Is.EqualTo("Index"));
+            Assert.That(viewResult.Model, Is.EqualTo(medicines));
+        }
+
     }
 }
